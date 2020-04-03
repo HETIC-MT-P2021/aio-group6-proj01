9d615df1228bdcf6981f623d22d1b550825f1089
oussama <oussama@oussama-Inspiron-5570>
2020-04-03 19:17:32 +0200

    update docker-compose.yml and create dockerfile create mysql service and create php service using dockerfile

--- docker/php/index.php
+++ docker/php/index.php
@@ -1,3 +1,30 @@
-<?php 
-	echo "Hello world!";
- ?>
+
+<?php 	
+
+$servername = "mysqldb";
+$username = "ig_project";
+$password = "root";
+$db = "ig_db_test";
+
+try{
+	$conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+
+if ($conn){
+	echo "Connected to server!";
+	}
+} catch (PDOException $e){
+	echo $e->getMessage()."<br>";
+	die();
+}
+
+// $sql = $conn->prepare("SELECT messages from logs_table");
+// $sql->execute();
+
+// echo "<br> <br>" . "Logs:" . "<br> <br>";
+
+// while($row = $sql->fetch()) {
+//         echo $row['messages'] . "<br>";
+//     }
+
+$conn = null;    
+?>
