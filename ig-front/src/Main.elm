9297693fba33fa06dea0a84d59bc382dccab4676
wyllisMonteiro <wyllismonteiro@gmail.com>
2020-04-12 23:31:24 +0200

    feat: group html thumbnails in home page
    Create renderThumbnails

--- ig-front/src/Main.elm
+++ ig-front/src/Main.elm
@@ -12,20 +12,60 @@ import Footer
 main =
   Browser.sandbox { init = 0, update = update, view = view }
 
--- Not useful for project
--- Delete when homepage is done
-type Msg = Increment | Decrement
 
--- Methods to change model
-update msg model =
+
+
+
+--update
+
+
+
+
+
+update : msg -> number -> number
+update msg num = num
+
+
+
+
+
+--view
+
+
+
+
+
+type ThumbnailsType
+  = ThumbnailsCategories
+  | ThumbnailsImages
+
+
+renderThumbnails : ThumbnailsType -> Html msg
+renderThumbnails msg = 
   case msg of
-    Increment ->
-      model + 1
+    ThumbnailsCategories ->
+      a [ href "#", class "home_categories_thumbnail" ] 
+        [ p [ class "home_category_name" ] [ text "Voiture" ]
+        , a [ href "#", class "icon_container" ] 
+            [ div [ class "icon icon_trash" ] [] ]
+        , a [ href "#", class "icon_container" ] 
+            [ div [ class "icon icon_pen" ] [] ]
+        ]
 
-    Decrement ->
-      model - 1
+    ThumbnailsImages ->
+      a [ href "#", class "home_images_thumbnail" ] 
+        [ div [ class "home_tags_images" ] 
+            [ span [ href "#", class "tag_thumbnails" ] [ text "Rouge" ]
+            , span [ href "#", class "tag_thumbnails" ] [ text "BMW" ]
+            ]
+        , a [ href "#", class "icon_container" ] 
+            [ div [ class "icon icon_trash" ] [] ]
+        , a [ href "#", class "icon_container" ] 
+            [ div [ class "icon icon_pen" ] [] ]
+        , a [ href "#", class "home_image_category" ] [ text "Voiture" ]
+        ]
+        
 
--- Show view
 view model =
   div []
     [ Navbar.main
@@ -33,27 +73,9 @@ view model =
         [ div [ class "home_categories_section" ] 
           [ h1 [] [ text "catégories" ],
             div [ class "home_categories_thumbnails" ] 
-              [ a [ href "#", class "home_categories_thumbnail" ] 
-                  [ p [ class "home_category_name" ] [ text "Voiture" ]
-                  ,  a [ href "#", class "icon_container" ] 
-                      [ div [ class "icon icon_trash" ] [] ]
-                  , a [ href "#", class "icon_container" ] 
-                      [ div [ class "icon icon_pen" ] [] ]
-                  ]
-              , a [ href "#", class "home_categories_thumbnail" ] 
-                    [ p [ class "home_category_name" ] [ text "Animaux" ],
-                    a [ href "#", class "icon_container" ] 
-                      [ div [ class "icon icon_trash" ] [] ],
-                    a [ href "#", class "icon_container" ] 
-                      [ div [ class "icon icon_pen" ] [] ]
-                  ]
-              , a [ href "#", class "home_categories_thumbnail" ]
-                  [ p [ class "home_category_name" ] [ text "Motos" ],
-                    a [ href "#", class "icon_container" ] 
-                      [ div [ class "icon icon_trash" ] [] ],
-                    a [ href "#", class "icon_container" ] 
-                      [ div [ class "icon icon_pen" ] [] ]
-                  ]
+              [ renderThumbnails ThumbnailsCategories
+              , renderThumbnails ThumbnailsCategories
+              , renderThumbnails ThumbnailsCategories
               ]
             , a [ href "#", class "link" ] [ text "Afficher toutes les catégories" ]
             , a [ href "#", class "link" ] [ text "+ Créer une nouvelle catégorie" ]
@@ -61,39 +83,9 @@ view model =
         , div [ class "home_images_section" ] 
             [ h1 [] [ text "images" ],
               div [ class "home_images_thumbnails" ] 
-                [ a [ href "#", class "home_images_thumbnail" ] 
-                    [ div [ class "home_tags_images" ] 
-                        [ span [ href "#", class "tag_thumbnails" ] [ text "Rouge" ]
-                        , span [ href "#", class "tag_thumbnails" ] [ text "BMW" ]
-                        ]
-                    , a [ href "#", class "icon_container" ] 
-                        [ div [ class "icon icon_trash" ] [] ]
-                    , a [ href "#", class "icon_container" ] 
-                        [ div [ class "icon icon_pen" ] [] ]
-                    , a [ href "#", class "home_image_category" ] [ text "Voiture" ]
-                    ]
-                , a [ href "#", class "home_images_thumbnail" ] 
-                    [ div [ class "home_tags_images" ] 
-                        [ span [ href "#", class "tag_thumbnails" ] [ text "Rouge" ]
-                        , span [ href "#", class "tag_thumbnails" ] [ text "BMW" ]
-                        ]
-                    , a [ href "#", class "icon_container" ] 
-                        [ div [ class "icon icon_trash" ] [] ]
-                    , a [ href "#", class "icon_container" ] 
-                        [ div [ class "icon icon_pen" ] [] ]
-                    , a [ href "#", class "home_image_category" ] [ text "Voiture" ]                      
-                  ]
-                , a [ href "#", class "home_images_thumbnail" ]
-                    [ div [ class "home_tags_images" ] 
-                        [ span [ href "#", class "tag_thumbnails" ] [ text "Rouge" ]
-                        , span [ href "#", class "tag_thumbnails" ] [ text "BMW" ]
-                        ]
-                    , a [ href "#", class "icon_container" ] 
-                        [ div [ class "icon icon_trash" ] [] ]
-                    , a [ href "#", class "icon_container" ] 
-                        [ div [ class "icon icon_pen" ] [] ]
-                    , a [ href "#", class "home_image_category" ] [ text "Voiture" ]                      
-                  ]
+                [ renderThumbnails ThumbnailsImages
+                , renderThumbnails ThumbnailsImages
+                , renderThumbnails ThumbnailsImages
                 ]
             , a [ href "#", class "link" ] [ text "Afficher toutes les images" ]
             , a [ href "#", class "link" ] [ text "+ Créer une nouvelle image" ]
