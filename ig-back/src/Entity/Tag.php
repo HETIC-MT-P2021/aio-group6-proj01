1f016593bd32d844fc41e906a4a1a67763422f23
valmrt77 <36480710+valmrt77@users.noreply.github.com>
2020-04-29 12:35:29 +0200

    fix: final final  serialization issue

--- ig-back/src/Entity/Tag.php
+++ ig-back/src/Entity/Tag.php
@@ -11,9 +11,9 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ApiResource(attributes={
- *     "normalization_context"={"groups"={"read"},
- *     "denormalizationContext={"groups"={"write"},
- *     "enable_max_depth"=true}}}
+ *     "normalization_context"={"groups"={"read"}},
+ *     "denormalizationContext={"groups"={"write"}},
+ *     "enable_max_depth"=true}
  *     )
  * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
  */
