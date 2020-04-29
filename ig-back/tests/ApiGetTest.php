028439d280eafe9d723d6585fe4b96cb178713f1
valmrt77 <36480710+valmrt77@users.noreply.github.com>
2020-04-29 16:35:22 +0200

    improvements on api unit tests

--- ig-back/tests/ApiGetTest.php
+++ ig-back/tests/ApiGetTest.php
@@ -10,23 +10,50 @@ class ApiGetTest extends WebTestCase {
 
         $client = static::createClient();
         $client->request('GET', '/api/categories');
+        $response = $client->getResponse();
+        $this->assertJsonResponse($response, 200);
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $content = json_decode($response->getContent(), true);
+        $this->assertInternalType('array', $content);
+        $this->assertCount(5, $content);
+
+        $comment = $content[0];
+        $this->assertArrayHasKey('body', $comment);
+        $this->assertArrayHasKey('status', $comment);
+        $this->assertArrayNotHasKey('content', $comment);
     }
 
     public function testGetImages() {
 
         $client = static::createClient();
         $client->request('GET', '/api/images');
+        $response = $client->getResponse();
+        $this->assertJsonResponse($response, 200);
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $content = json_decode($response->getContent(), true);
+        $this->assertInternalType('array', $content);
+        $this->assertCount(7, $content);
+
+        $comment = $content[0];
+        $this->assertArrayHasKey('body', $comment);
+        $this->assertArrayHasKey('status', $comment);
+        $this->assertArrayNotHasKey('content', $comment);
     }
 
     public function testGetTags() {
 
         $client = static::createClient();
         $client->request('GET', '/api/tags');
+        $response = $client->getResponse();
+        $this->assertJsonResponse($response, 200);
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $content = json_decode($response->getContent(), true);
+        $this->assertInternalType('array', $content);
+        $this->assertCount(5, $content);
+
+        $comment = $content[0];
+        $this->assertArrayHasKey('body', $comment);
+        $this->assertArrayHasKey('status', $comment);
+        $this->assertArrayNotHasKey('content', $comment);
     }
 }
